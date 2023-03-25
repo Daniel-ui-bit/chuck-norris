092119301159ab18f55d7d5d87e8815964cc6d54
Bapeppe <>
2023-03-25 11:33:15 +0100

    Sistemazione App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './style/App.css'
-import Button from './components/button'
+import Button from './components/Button'
 import Dropdown from './components/Dropdown'
 
 
